95f7b4c27a700d362dc4addc151104066c8b0b45
spiritcoder666 <144348608+spiritcoder666@users.noreply.github.com>
2025-01-09 15:47:18 +0530

    [35] [Time Beats: 100.00%] [Memory Beats: 28.89%] - LeetPush

--- 35-Search-Insert-Position.cpp
+++ 35-Search-Insert-Position.cpp
@@ -1,9 +1,21 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        for (int i = 0; i < nums.size(); i++)
-         if (target <= nums[i]){
-         return i;}
-        return nums.size();
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;        
     }
 };
