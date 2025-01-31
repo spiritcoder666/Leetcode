68c07e2be9b3e944793435b1f6fa955381df0e1a
Rohit_Mehta <144348608+spiritcoder666@users.noreply.github.com>
2025-01-31 21:48:22 +0530

    [509] [Time Beats: 78.69%] [Memory Beats: 62.75%] - LeetPush

--- 509-Fibonacci-Number.cpp
+++ 509-Fibonacci-Number.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        else if(n==1) return 1;
-        else return fib(n-1)+fib(n-2);
-        
+       int f[n+1];
+       if(n==0 || n==1) return n;
+       f[0] =0;
+       f[1]=1;
+       for (int i=2;i<=n;i++){
+        f[i]=f[i-1]+f[i-2];
+       } 
+       return f[n];       
     }
 };
