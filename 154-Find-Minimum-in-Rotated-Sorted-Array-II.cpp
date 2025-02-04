094e1daac6b8015e2d1a360e317baf61781f0197
Rohit_Mehta <144348608+spiritcoder666@users.noreply.github.com>
2025-02-04 13:38:30 +0530

    [154] [Time Beats: 100.00%] [Memory Beats: 29.42%] - LeetPush

---
+++ 154-Find-Minimum-in-Rotated-Sorted-Array-II.cpp
@@ -0,0 +1,19 @@
+/*
+By  :-: savetrees
+Used:-: Binary Search After Removal of Duplicates
+*/
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+    int low=0;int high=nums.size()-1;
+    while(low<high)
+    {
+        while(low<high && nums[low]==nums[high]) high--;
+        //Removal of Duplicates
+        int mid=low+(high-low)/2;
+        if(nums[mid]>nums[high])low=mid+1;
+        else high=mid;
+    }
+    return nums[low];     
+    }
+};
