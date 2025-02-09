8176fdfb356b08669472ab7a62b3f0fac39a73f4
Rohit_Mehta <144348608+spiritcoder666@users.noreply.github.com>
2025-02-09 18:33:40 +0530

    [367] [Time Beats: 100.00%] [Memory Beats: 18.20%] - LeetPush

---
+++ 367-Valid-Perfect-Square.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        long long x = num;
+    while (x * x > num) {
+        x = (x + num / x) / 2;
+    }
+    return x * x == num;
+
+    }
+};
