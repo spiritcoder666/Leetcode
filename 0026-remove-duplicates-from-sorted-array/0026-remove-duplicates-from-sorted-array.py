76ca34c365277c46709b2d25c80cf0f4625b8757
Rohit_Mehta <144348608+spiritcoder666@users.noreply.github.com>
2025-12-22 00:56:09 +0530

    Time: 3 ms (47.86%), Space: 18.3 MB (98.53%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,15 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         n=len(nums)
-        freq_map={}
-        for i in range(0,n):
-            freq_map[nums[i]]=0
-        j=0
-        for k in freq_map:
-            nums[j]=k
+        if n==1:
+            return 1
+        i=0
+        j=i+1
+        while j<n:
+            if nums[j]!=nums[i]:
+                i+=1
+                nums[i],nums[j]=nums[j],nums[i]
             j+=1
-        return j
+        return i+1
+
+        
